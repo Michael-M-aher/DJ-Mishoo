32c5da04e7179d7cd86676afca65a6fe7184397d
Michael Maher <michael.maher.nagi@gmail.com>
2022-06-18 06:11:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,34 @@ const ffmpeg = require("ffmpeg-static");
 const voice = require("@discordjs/voice");
 const DisTube = require("distube").default;
 const https = require('https-proxy-agent');
-const { exec } = require('child_process')
-//use "kill 1" in shell if stopped
 const client = new Discord.Client({
+    fetchAllMembers: false,
+    //restTimeOffset: 0,
+    //restWsBridgetimeout: 100,
     shards: "auto",
+    //shardCount: 5,
     allowedMentions: {
       parse: [ ],
       repliedUser: false,
     },
+    failIfNotExists: false,
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
     intents: [ 
         Discord.Intents.FLAGS.GUILDS,
+        //Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
         Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        //Discord.Intents.FLAGS.GUILD_BANS,
+        //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+        //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+        //Discord.Intents.FLAGS.GUILD_INVITES,
+        //Discord.Intents.FLAGS.GUILD_PRESENCES,
+        //Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
     ],
     presence: {
       activity: {
@@ -32,7 +47,8 @@ const client = new Discord.Client({
     }
 });
 
-const proxy = 'http://75.111.123.167:1888';
+
+const proxy = 'http://123.123.123.123:8080';
 const agent = https(proxy);
 const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
@@ -50,24 +66,23 @@ client.distube = new DisTube(client, {
   emitNewSongOnly: false,
   leaveOnEmpty: true,
   leaveOnFinish: false,
-  leaveOnStop: false,
-  emptyCooldown: 120,
+  leaveOnStop: true,
   savePreviousSongs: true,
   emitAddSongWhenCreatingQueue: false,
   //emitAddListWhenCreatingQueue: false,
   searchSongs: 0,
   youtubeCookie: config.youtubeCookie,     //Comment this line if you dont want to use a youtube Cookie 
-  nsfw: true, //Set it to false if u want to disable nsfw songs
-  emptyCooldown: 60,
+  nsfw: false, //Set it to false if u want to disable nsfw songs
+  emptyCooldown: 25,
   ytdlOptions: {
-    //requestOptions: {
-    //  agent //ONLY USE ONE IF YOU KNOW WHAT YOU DO!
-    //},
+    requestOptions: {
+      agent
+    },
     highWaterMark: 1024 * 1024 * 64,
     quality: "highestaudio",
     format: "audioonly",
     liveBuffer: 60000,
-    dlChunkSize: 1024 * 1024 * 64,
+    dlChunkSize: 1024 * 1024 * 4,
   },
   youtubeDL: true,
   updateYouTubeDL: true,
@@ -84,25 +99,23 @@ client.slashCommands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.categories = require("fs").readdirSync(`./commands`);
 client.allEmojis = require("./botconfig/emojis.json");
+client.maps = new Map();
 
 client.setMaxListeners(100); require('events').defaultMaxListeners = 100;
 
 client.settings = new Enmap({ name: "settings",dataDir: "./databases/settings"});
 client.infos = new Enmap({ name: "infos", dataDir: "./databases/infos"});
-
+client.autoresume = new Enmap({ name: "autoresume", dataDir: "./databases/infos"});
 
 //Require the Handlers                  Add the antiCrash file too, if its enabled
 ["events", "commands", "slashCommands", settings.antiCrash ? "antiCrash" : null, "distubeEvent"]
     .filter(Boolean)
     .forEach(h => {
         require(`./handlers/${h}`)(client);
-    })|| config.token
+    })
 //Start the Bot
-try {
-  client.login(process.env.tokens)
-} catch(ex) {
-  exec("kill 1")
-}
+client.login(config.token)
+
 
 
 
