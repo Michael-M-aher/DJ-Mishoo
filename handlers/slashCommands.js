ffad38456e45ef7846dc187d6e6dc7d21e4d315a
Michael Maher <michael.maher.nagi@gmail.com>
2022-06-22 00:32:26 +0200

    Update slashCommands.js

--- handlers/slashCommands.js
+++ handlers/slashCommands.js
@@ -157,19 +157,19 @@ module.exports = (client) => {
 			}
 		})
 		//DISABLE WHEN USING GLOBAL!
-		// client.on("guildCreate", async (guild) => {
-		// 	try{
-		// 		if(!config.loadSlashsGlobal){
-		// 			await guild.commands.set([]).catch((e)=>{});
-		// 			guild.commands.set(allCommands)
-		// 				.then(slashCommandsData => {
-		// 					console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.green} Loaded for: ${`${guild.name}`.underline}`.brightGreen); 
-		// 				}).catch((e)=>{});
-		// 		}
-		// 	}catch (e){
-		// 		console.log(String(e).grey)
-		// 	}
-		// })
+		client.on("guildCreate", async (guild) => {
+			try{
+				if(!config.loadSlashsGlobal){
+					await guild.commands.set([]).catch((e)=>{});
+					guild.commands.set(allCommands)
+						.then(slashCommandsData => {
+							console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.green} Loaded for: ${`${guild.name}`.underline}`.brightGreen); 
+						}).catch((e)=>{});
+				}
+			}catch (e){
+				console.log(String(e).grey)
+			}
+		})
 		
     } catch (e) {
         console.log(String(e.stack).bgRed)
